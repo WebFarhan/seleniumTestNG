607d95ee59b6ccbcf906be860621bd97bcea6eba
Razin Hussain <razin.farhan2018@gmail.com>
2025-02-27 12:19:54 -0500

    Add files via upload
    initial commit

---
+++ src/test/basicTests.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class basicTests {
+
+	@Test
+	public void Demo() {
+		System.out.println("hello");
+	}
+
+	@Test
+	public void secondTest() {
+		System.out.println("Second test");
+	}
+}
